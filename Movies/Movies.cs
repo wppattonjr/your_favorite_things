4119b4c6ab20d73df24d3b279fcb9b9d9a8f9835
Wendell Patton Jr <wendell.pattonjr@gmail.com>
2021-01-23 10:18:01 -0600

    your favorite things exercise

---
+++ Movies/Movies.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace YourFavoriteThings.Movies
+{
+    public class Movie
+    {
+            //Properties
+            public string Name { get; set; }
+            public string Director { get; set; }
+            public int Released { get; set; }
+            public string Genre { get; set; }
+
+            //Fields - internal private things
+            private bool _wonOscar = false;
+
+
+            //Constructor
+            public Movie()
+            {
+
+            }
+
+            //Method
+            public void Win() {
+                if(_wonOscar == false)
+                {
+                    Console.WriteLine("The movie did not win an Oscar.");
+                  
+                } else
+                {
+                    Console.WriteLine("This movie won an Oscar!!");
+                                    
+                }
+            
+                }
+
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using YourFavoriteThings.Movies;
+
+namespace YourFavoriteThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movie1 = new Movie();
+            {
+                movie1.Name = "Do the Right Thing";
+                movie1.Director = "Spike Lee";
+                movie1.Genre = "Comedy | Drama";
+                movie1.Released = 1989;
+                movie1.Win();
+                Console.WriteLine($"{movie1.Name} was released in {movie1.Released}. It was directed by {movie1.Director} and is considered to be a {movie1.Genre}.");
+            }
+
+            var movie2 = new Movie();
+            {
+                movie2.Name = "The Matrix";
+                movie2.Director = "The Wachowski Brothers";
+                movie2.Genre = "Action | Sci-Fi";
+                movie2.Released = 1999;
+                movie2.Win();
+                Console.WriteLine($"{movie2.Name} was released in {movie2.Released}. It was directed by {movie2.Director} and is considered to be a {movie2.Genre}.");
+            }
+        }
+    }
+}
